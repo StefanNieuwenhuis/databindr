f365dc8830e2a92fe561361f157657cd7e033234
Stefan Nieuwenhuis <StefanNieuwenhuis@users.noreply.github.com>
2019-10-07 09:47:27 +0200

    Update flatten.ts
    Add method description (TSDoc style)

--- src/utils/flatten.ts
+++ src/utils/flatten.ts
@@ -1,5 +1,6 @@
 export class Flatten {
   private _result: any = {};
+
   private _recurse(cur: any, prop: string) {
     if (this._isObject(cur)) {
       let isEmpty = true;
@@ -15,6 +16,12 @@ export class Flatten {
     }
   }
 
+  /**
+   * This method checks if a given input is of type Object
+   *
+   * @param object - Any given input to be checked
+   * @returns True when the input is of type Object and false if it isn't
+   */
   private _isObject(object: any): boolean {
     return Object.prototype.toString.call(object).indexOf("Object") > -1;
   }
